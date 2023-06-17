3de1ea8278e22b2bb30b85d0d4c0d494ac148b8a
Leo Zarazaga <lza1313@outlook.com>
2023-06-17 16:21:09 +0200

    refactoring code

--- Modal/script.js
+++ Modal/script.js
@@ -1,3 +1,5 @@
+// REFACTORING CODE
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const buttonsOpenModal = document.querySelectorAll('.show-modal');
@@ -24,4 +26,34 @@ document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal()
     }
-});
+}); 
+
+
+
+
+// BEFORE REFACTORING
+
+/* 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonsOpenModal = document.querySelectorAll('.show-modal');
+const buttonCloseModal = document.querySelector('.close-modal');
+
+
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+    buttonsOpenModal[i].addEventListener('click', () => {
+        overlay.classList.remove('hidden');
+        modal.classList.remove('hidden');
+    })
+}
+
+buttonCloseModal.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+}); */
