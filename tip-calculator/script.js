7bc9f50df190f642bd7cf206ee67e2ce8f9f9c37
Leo Zarazaga <lza1313@outlook.com>
2023-06-18 18:36:08 +0200

    improved my tip calculator

---
+++ tip-calculator/script.js
@@ -0,0 +1,18 @@
+const billElement = document.getElementById('bill');
+const tipElement = document.getElementById('tip');
+const calculateButton = document.getElementById('calculate');
+const displayTotal = document.getElementById('total');
+
+let calculateTip = () => {
+    let billValue = parseFloat(billElement.value);
+    let tipValue = parseFloat(tipElement.value);
+
+    let totalValue = billValue + (billValue * (tipValue / 100));
+
+    displayTotal.textContent = totalValue.toFixed(2)
+
+    console.log(billValue);
+    console.log(tipValue);
+}
+
+calculateButton.addEventListener('click', calculateTip)
