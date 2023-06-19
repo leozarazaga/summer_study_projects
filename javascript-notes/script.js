9b37f8969ebe6f59de26883804a524f17573887b
Leo Zarazaga <lza1313@outlook.com>
2023-06-19 20:31:26 +0200

    practicing more on scoping

--- javascript-notes/script.js
+++ javascript-notes/script.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+// - - - - - - - - - - - - - - - - - SCOPING - - - - - - - - - - - - - - - - - 
+
+const firstname = "Leo";
+
+function calcAge(birthyear) {
+    const age = 2023 - birthyear;
+
+    function printAge() {
+        const output = `${firstname}, you're ${age}, born in ${birthyear}`
+        console.log(output)
+
+        if (birthyear >= 1981 && birthyear <= 1996) {
+            const str = `Oh, and you're a millenial, ${firstname}`
+            console.log(str);
+        }
+        function add(a, b) {
+            return a + b;
+        }
+
+        console.log(add(2, 3));
+    }
+    
+    
+    printAge()
+}
+
+calcAge(1990)
