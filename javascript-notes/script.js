b6955b1e7d92c08ad92940f87bc459b9c6f50f4b
Leo Zarazaga <lza1313@outlook.com>
2023-06-20 22:28:32 +0200

    destructuring objects

--- javascript-notes/script.js
+++ javascript-notes/script.js
@@ -231,8 +231,59 @@ const restaurant = {
     starterMenu: ['Foccacia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+    openingHours: {
+        thursday: {
+            open: 12,
+            close: 22,
+        },
+        friday: {
+            open: 11,
+            close: 23,
+        },
+        saturday: {
+            open: 0,
+            close: 24,
+        }
+    },
+
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-    }
+    },
+
+    orderDelivery: function({starterIndex, mainIndex, time, address}){
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
 }
 
+
+restaurant.orderDelivery({
+    time: "22:30",
+    address: 'Via del Sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+})
+
+//const {name, openingHours, categories} = restaurant;
+
+//console.log(name, openingHours, categories);
+
+//const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+//console.log(restaurantName, hours, tags);
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+//Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj)
+// console.log(a, b);
+
+//Nested objects 
+//const { friday: {open: o, close: c}, } = openingHours;
+//console.log(o, c);
+
+// const{friday : {open, close}} = openingHours;;
+// console.log(open, close);
