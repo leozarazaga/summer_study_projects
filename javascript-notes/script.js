ec4f819460060590eb9d6b5623f4717d194cfad3
Leo Zarazaga <lza1313@outlook.com>
2023-06-20 15:03:12 +0200

    learning more about hosting and the THIS keyword

--- javascript-notes/script.js
+++ javascript-notes/script.js
@@ -1,6 +1,6 @@
 // - - - - - - - - - - - - - - - - - SCOPING - - - - - - - - - - - - - - - - - 
 
-const firstname = "Leo";
+/* const firstname = "Leo";
 
 function calcAge(birthyear) {
     const age = 2023 - birthyear;
@@ -19,9 +19,45 @@ function calcAge(birthyear) {
 
         console.log(add(2, 3));
     }
-    
-    
     printAge()
 }
 
-calcAge(1990)
+calcAge(1990) */
+
+// - - - - - - - - - - - - - - - - - HOSTING - - - - - - - - - - - - - - - - - 
+
+/* console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = "Jonas";
+let job = 'teacher';
+const year = 1991;
+
+//Functions
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+//Example
+console.log(undefined);
+
+if(!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart(){
+    console.log('All products deleted!');
+} */
+
+
+// - - - - - - - - - - - - - - - - - THIS - - - - - - - - - - - - - - - - - 
