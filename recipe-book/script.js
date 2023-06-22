1ab26a81a9964148674a639f0f7a0eff794e5689
Leo Zarazaga <lza1313@outlook.com>
2023-06-22 23:15:53 +0200

    fixing the recipe book API

--- recipe-book/script.js
+++ recipe-book/script.js
@@ -1,9 +1,40 @@
-async function getRecipes(){
+ const API_KEY = "275d58779ccf4e22af03e792e8819fff";
+const recipeListElement = document.getElementById('recipe-list')
+
+function displayRecipes(recipes){
+    recipeListElement.innerHTML = "";
+
+    recipes.forEach((recipe) =>{
+        const recipeItemElement = document.createElement('li');
+        recipeItemElement.classList.add('recipe-item');
+        
+        recipeImageElement = document.createElement('img');
+        recipeImageElement.src = recipe.image;
+        recipeImageElement.alt = 'recipe image'
+
+        recipeItemElement.appendChild(recipeImageElement)
+
+        recipeListElement.appendChild(recipeItemElement)
+    })
     
 }
 
-async function init(){
-    const recipes = await getRecipes()
+async function getRecipes() {
+   const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`);
+  const data = await response.json();
+
+  return data.recipes;
 }
 
-init()
+async function init() {
+  const recipes = await getRecipes();
+
+ displayRecipes(recipes)
+  
+}
+
+init(); 
+
+//NOT DONE
+//FIX THE API REQUEST
+
