88a9328575a37d44ec623bf81f7d3051bd02c260
Leo Zarazaga <lza1313@outlook.com>
2023-07-07 23:35:52 +0200

    created MapAPI class to encapsulate map-related functionality

---
+++ mapty/script.js
@@ -0,0 +1,197 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+class Workout {
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+
+    constructor(coordinates, distance, duration) {
+        this.coordinates = coordinates; // [lat, lng]
+        this.distance = distance; //in km
+        this.duration = duration; // in min
+    }
+}
+
+class Running extends Workout {
+    type = 'running'
+    constructor(coordinates, distance, duration, cadence) {
+        super(coordinates, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace() {
+        // (min/km)
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+
+
+class Cycling extends Workout {
+    type = 'cycling';
+    constructor(coordinates, distance, duration, elevationGain) {
+        super(coordinates, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed()
+    }
+    calcSpeed() {
+        // (km/h)
+        this.speed = this.distance / (this.duration / 60)
+        return this.speed;
+    }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178)
+// const cycling1 = new Cycling([39, -12], 27, 90, 523)
+// console.log(run1, cycling1);
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//                              APPLICATION ARCHITECTURE
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+class App {
+    #map;
+    #mapEvent;
+    #workouts = [];
+
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this))
+        inputType.addEventListener('change', this._toggleElevationField)
+    };
+
+
+    _getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                alert('Could not get your position!');
+            });
+        }
+    }
+
+    _loadMap(position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+        const coordinates = [latitude, longitude];
+
+        this.#map = L.map('map').setView(coordinates, 13);
+        // console.log(map);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+
+        //Handling clicks on map
+        this.#map.on('click', this._showForm.bind(this))
+
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e) {
+
+        const validInputs = (...inputs) => inputs.every((input) => Number.isFinite(input))
+        const allPositive = (...inputs) => inputs.every((inp) => inp > 0)
+
+        e.preventDefault();
+
+        //Get data from the form
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+        const { lat, lng } = this.#mapEvent.latlng;
+        let workout;
+
+        //If workout running, create running object
+        if (type === 'running') {
+            const cadence = +inputCadence.value;
+
+            //Check if data is valid
+            if (!validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence)) {
+                 alert('Inputs have to be positive numbers!')
+            }
+
+            workout = new Running([lat, lng], distance, duration, cadence);
+        }
+
+
+        //If workout cycling, create cycling object
+        if (type === 'cycling') {
+            const elevation = +inputElevation.value;
+
+            //Check if data is valid
+            if (!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) {
+                alert('Inputs have to be positive numbers!')
+            }
+
+             workout = new Cycling([lat, lng], distance, duration, elevation);
+          
+        }
+
+        //Add the new object to workout array
+        this.#workouts.push(workout)
+        console.log(workout);
+
+        //Render workout on map as marker
+       
+
+        //Render workout on list
+        this.renderWorkoutMarker(workout)
+        //Hide from + clear input fields
+
+
+        //Clear input fields
+        inputDistance.value = '';
+        inputCadence.value = '';
+        inputDuration.value = '';
+        inputElevation.value = '';
+
+
+    }
+
+    renderWorkoutMarker(workout){
+        L.marker(workout.coordinates).addTo(this.#map).bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoclose: false,
+                closeOnClick: false,
+                className: `${workout.type}-popup`,
+            })
+        ).setPopupContent('workout').openPopup();
+    }
+}
+
+
+const app = new App();
+
+
+
+
+
+
+
